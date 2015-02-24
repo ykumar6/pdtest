dab941fa7a49738e70e3815c7f469385eff9e751
Paul Duffy <paulrduffy@gmail.com>
2015-02-23 21:07:07 -0800

    Adjusted

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations!</h2>
-                <p>Your PHP application is now running in runnable &ldquo;<?php echo gethostname(); ?>&rdquo; in your own dedicated environment in the AWS&nbsp;Cloud.</p>
+                <p>Your PHP application is now running in runnable &ldquo;<?php echo gethostname(); ?>&rdquo; in some &nbsp;Cloud.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
             </div>
         </div>
